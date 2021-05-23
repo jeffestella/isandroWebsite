d533e13b0d2645df454e4fa96750336aeb97de7a
jeffestella <jeffrey.estella@gmail.com>
2021-05-22 19:04:26 -0700

    test pagination

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
 module.exports = {
-    markdownTemplateEngine: "11ty.js"
+    markdownTemplateEngine: "11ty.js",
+    templateFormats: ["11.js","md"]
 };
--- writing/writing.11ty.js
+++ writing/writing.11ty.js
@@ -1,13 +1,25 @@
 exports.data = {
     title: "Selected Works",
-    layout: "layout-base.11ty.js"
+    pagination: {
+        data: this.notionContent,
+        size: 2
+    }
 }
 
 exports.render = (data) => {
     const publishedWriting = [];
+    for (post of data.notionContent) {
+        if (post.fields.published) {
+            publishedWriting.push(
+                `<li>${post.fields.title}<p>${post.fields.text}</p></li>`
+            )
+        }
+    }
     
 
     return `
-    <p>Lorem</p>
+    <ul>
+    ${publishedWriting.join('')}
+    </ul>
     `
 }
