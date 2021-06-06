32c8000f291e1a1762fa62f710fec93aaaf80c51
jeffestella <jeffrey.estella@gmail.com>
2021-06-06 13:13:47 -0700

    add /src/assets dir. finish styles for writing

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/css/");
     return {
         dir: {
             input: "src",
             output: "_site"
         },
-        templateFormats: ["html", "md", "liquid"],
+        templateFormats: ["html", "md", "liquid","png"],
         markdownTemplateEngine: "liquid",
         htmlTemplateEngine: "liquid"
     };
